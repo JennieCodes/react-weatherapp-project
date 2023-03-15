4d3d997a1cab1a9c00b77e48c3f3ac495ef7bb17
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-03-15 10:32:05 -0400

    Added components

---
+++ src/Api.js
@@ -0,0 +1,4 @@
+export default class Api {
+  static url = "https://api.openweathermap.org";
+  static key = "275a753ef1dcfe59aa4a1d07e378894a";
+}
--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Navigation from "./Navigation";
+import Main from "./Main";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Main />
+      </div>
+    );
+  }
+}
